71237ac94b73e56a38ca55f3d118e4adc95f400d
chaitrali17042000 <chaitralimirashi@gmail.com>
2026-03-03 23:21:55 +0530

    Setup added for payment-ingestion Spring Boot skeleton and link common-schemas

---
+++ payment-ingestion/src/main/java/com/bank/ingestion/PaymentIngestionApplication.java
@@ -0,0 +1,12 @@
+package com.bank.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIngestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentIngestionApplication.class, args);
+    }
+}
